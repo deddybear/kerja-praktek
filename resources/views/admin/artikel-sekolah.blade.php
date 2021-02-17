dbc5e6ae5f40252f99f2cd13ff8d34e021bf9bbe
phuney <nva3.vaaa@gmail.com>
2021-02-17 23:47:52 +0700

    fix modal dismiss

--- resources/views/admin/artikel-sekolah.blade.php
+++ resources/views/admin/artikel-sekolah.blade.php
@@ -59,7 +59,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Tambah Artikel</h5>
-                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
             </div>
@@ -87,7 +87,7 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     <button type="submit" id="tombol-submit" class="btn btn-primary"></button>
                 </div>
             </form>
